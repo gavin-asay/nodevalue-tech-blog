b7829cb2d973d2d1ce829fe85ed2754bbd856fcd
Gavin Asay <gavin.asay@gmail.com>
2021-02-20 03:30:25 -0700

    models

---
+++ models/Comment.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const marked = require('marked');
+const sanitizeHtml = require('sanitize-html');
+
+class Comment extends Model {}
+
+Comment.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		comment_text: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		user_id: {
+			user_id: {
+				type: DataTypes.INTEGER,
+				references: {
+					model: 'user',
+					key: 'id',
+				},
+			},
+		},
+		post_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'post',
+				key: 'id',
+			},
+		},
+	},
+	{
+		sequelize,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'comment',
+		hooks: {
+			beforeCreate(newCommentData) {
+				newCommentData.comment_text = sanitizeHtml(marked(newCommentData.comment_text));
+				return newCommentData;
+			},
+			beforeUpdate(updatedCommentData) {
+				updatedCommentData.comment_text = sanitizeHtml(marked(updatedCommentData.comment_text));
+				return updatedCommentData;
+			},
+		},
+	}
+);
---
+++ models/Post.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const marked = require('marked');
+const sanitizeHtml = require('sanitize-html');
+
+class Post extends Model {}
+
+Post.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		title: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [5],
+			},
+		},
+		body: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+			validate: {
+				len: [10],
+			},
+		},
+		user_id: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'user',
+				key: 'id',
+			},
+		},
+	},
+	{
+		sequelize,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'post',
+		hooks: {
+			beforeCreate(newPostData) {
+				newPostData.body = sanitizeHtml(marked(newPostData.body));
+				return newPostData;
+			},
+			beforeUpdate(updatedPostData) {
+				updatedPostData.body = sanitizeHtml(marked(updatedPostData.body));
+				return updatedPostData;
+			},
+		},
+	}
+);
+
+module.exports = Post;
